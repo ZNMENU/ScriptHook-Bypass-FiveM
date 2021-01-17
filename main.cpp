b12df468833f5e0010068b44c909676c934446d0
ZN MENU.LUA <73449099+ZNMENU@users.noreply.github.com>
2021-01-17 13:11:36 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 
   while (true)
     {
-        *(BYTE*)(CustomAPI::GetModuleA("gta-core-five") + 0xD9858) = 1; //Update the offsets
+        *(BYTE*)(CustomAPI::GetModuleA("gta-core-five") + 0xD9858) = 1; //Update the offsets UPDATED 16/01/2021
         
     }
     return 0;
